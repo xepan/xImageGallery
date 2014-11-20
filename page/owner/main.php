4b82765695834cef75fc45d092dd802bbf3111f4
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-11-20 14:54:56 +0530

    Icon added

--- page/owner/main.php
+++ page/owner/main.php
@@ -2,8 +2,9 @@
 
 class page_xImageGallery_page_owner_main extends page_componentBase_page_owner_main {
 	function page_index(){
-		$this->add('H3')->setHTML('Component Owner Main Page');
-		
+
+		$this->h1->setHTML('<i class="fa fa-file-picture-o"></i> '.$this->component_name. '<small>Different kinds of Attractive and Custom Image Gallery</small>');
+
 		$tab=$this->add('Tabs');
 		$cat_tab=$tab->addTabURL('xImageGallery/page_owner_imageGallery','Google Gallery');
 	}
